ad17760adff7a6fab91580a8a9097ca08340382d
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-12-07 18:29:42 -0700

    added resolvers for dog and owners

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,19 +5,12 @@ const resolvers = {
     Query: {
       users: async () => {
         return User.find()
-        // .populate('thoughts');
+        .populate('thoughts');
       },
       user: async (parent, { username }) => {
         return User.findOne({ username })
-        // .populate('thoughts');
+        .populate('thoughts');
       },
-      // thoughts: async (parent, { username }) => {
-      //   const params = username ? { username } : {};
-      //   return Thought.find(params).sort({ createdAt: -1 });
-      // },
-      // thought: async (parent, { thoughtId }) => {
-      //   return Thought.findOne({ _id: thoughtId });
-      // },
       dogs: async () => {
         return Dogs.find()
         // .populate('dogs');
@@ -25,22 +18,21 @@ const resolvers = {
       dog: async (parent, { dogsId }) => {
         return Dogs.findOne({ _id: dogsId });
       },
-      // owners: async () => {
-      //   return owners.find().populate('owners');
-      // },
-      // owner: async (parent, { ownerId }) => {
-      //   return owner.findOne({ _id: ownerId });
-      // },
-      // rooms: async () => {
-      //   return rooms.find().populate('rooms');
-      // },
-      // room: async (parent, { roomId }) => {
-      //   return room.findOne({ _id: roomId });
-      // },
+      owners: async () => {
+        return owners.find().populate('owners');
+      },
+      owner: async (parent, { ownerId }) => {
+        return owner.findOne({ _id: ownerId });
+      },
+      rooms: async () => {
+        return rooms.find().populate('rooms');
+      },
+      room: async (parent, { roomId }) => {
+        return room.findOne({ _id: roomId });
+      },
       me: async (parent, args, context) => {
         if (context.user) {
           return User.findOne({ _id: context.user._id })
-          // .populate('thoughts');
         }
         throw AuthenticationError;
       },
@@ -54,54 +46,48 @@ const resolvers = {
       },
       login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
-  
         if (!user) {
           throw AuthenticationError;
         }
-  
         const correctPw = await user.isCorrectPassword(password);
-  
         if (!correctPw) {
           throw AuthenticationError;
         }
-  
         const token = signToken(user);
   
         return { token, user };
       },
-      // addThought: async (parent, { thoughtText }, context) => {
-      //   if (context.user) {
-      //     const thought = await Thought.create({
-      //       thoughtText,
-      //       thoughtAuthor: context.user.username,
-      //     });
+      addDog: async (parent, { dogName }, context) => {
+        if (context.owner) {
+          const dog = await Dog.create({
+            dogName,
+          });
   
-      //     await User.findOneAndUpdate(
-      //       { _id: context.user._id },
-      //       { $addToSet: { thoughts: thought._id } }
-      //     );
+          await owner.findOneAndUpdate(
+            { _id: context.owner._id },
+            { $addToSet: { dogs: dog._id } }
+          );
   
-      //     return thought;
-      //   }
-      //   throw AuthenticationError;
-      //   ('You need to be logged in!');
-      // },
-      // removeThought: async (parent, { thoughtId }, context) => {
-      //   if (context.user) {
-      //     const thought = await Thought.findOneAndDelete({
-      //       _id: thoughtId,
-      //       thoughtAuthor: context.user.username,
-      //     });
+          return dogs;
+        }
+        throw AuthenticationError;
+        ('You need to be logged in!');
+      },
+      removeDog: async (parent, { dogsId }, context) => {
+        if (context.owner) {
+          const dog = await Dog.findOneAndDelete({
+            _id: dogId,
+          });
   
-      //     await User.findOneAndUpdate(
-      //       { _id: context.user._id },
-      //       { $pull: { thoughts: thought._id } }
-      //     );
+          await Owner.findOneAndUpdate(
+            { _id: context.owner._id },
+            { $pull: { dogs: dog._id } }
+          );
   
-      //     return thought;
-      //   }
-      //   throw AuthenticationError;
-      // },
+          return dogs;
+        }
+        throw AuthenticationError;
+      },
     },
   };
   
