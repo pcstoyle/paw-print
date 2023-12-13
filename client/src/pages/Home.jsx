02ae97ca069a71af6865e0726e1dda8d3c94a607
mono <traineralaina@gmail.com>
2023-12-12 20:13:45 -0700

    addOwner works now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,46 @@
+import React, { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_DOGS } from '../../utils/queries';
 import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react';
-function Home() {
-    return (
-      <section>
-        <div className='my-4'>
-        <button ><Link to='/addDog'>
-      Add a New Dog
-      </Link></button>
-      </div>
-      <div className='my-4'>
-        <button ><Link to='/addOwner'>
-      Add a New Owner
-      </Link></button>
-      </div>
-      <div>
-      <input type="search" placeholder="search"/>
-      <button> search</button></div>
-      </section>
 
-     
-    );
-  }
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchDogs, { loading, error, data }] = useLazyQuery(QUERY_DOGS);
+
+  const handleSearch = () => {
+    
+    searchDogs({ variables: { searchTerm } });
+    console.log(searchTerm);
+  };
+
+  return (
+    <div>
+      <Link to="/addOwner">
+        <button>Add an Owner</button>
+      </Link>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search dogs..."
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && data.searchDogs && data.searchDogs.length === 0 && (
+  <p>No dogs found with that information.</p>
+)}
+      {data && data.searchDogs && data.searchDogs.map((dog) => (
+  <div key={dog.id}>
+    <p>Name: {dog.name}</p>
+    <p>Breed: {dog.breed}</p>
+    {/* Display other dog details */}
+  </div>
+))}
+    </div>
+  );
+};
   
   export default Home;
+
